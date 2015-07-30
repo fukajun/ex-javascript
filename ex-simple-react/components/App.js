2203674eeaddde51504a024c6cefda4754254f4d
J.Fukaya <fukajun.shark@gmail.com>
2015-07-30 22:12:40 +0900

    Add simple-react sample

---
+++ ex-simple-react/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+export class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+  change(e) {
+    this.setState({ counter:  e.target.value })
+  }
+
+  incrementCounter() {
+    var counter = this.state.counter + 1
+    this.setState({ counter:  counter })
+  }
+
+  render() {
+    return (
+        <div>
+        <section>
+        <span>{this.state.counter}</span>
+        <br />
+        <input type='text' onChange={this.change}  value={this.state.counter} />
+        <input type='button' value='Add' onClick={this.incrementCounter.bind(this)}/>
+        </section>
+        </div>
+    )
+  }
+}
---
+++ ex-simple-react/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { App } from './components/App'
+
+React.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('target')
+)
