d65634af4466a09cb9dbdc1552a78bc0ecb68459
J.Fukaya <fukajun.shark@gmail.com>
2015-08-18 20:19:21 +0900

    Start implement redux

--- ex-redux-validation/components/App.js
+++ ex-redux-validation/components/App.js
@@ -1,47 +1,28 @@
 import React from 'react'
 import _ from 'lodash'
-import NameInput from './NameInput'
 import Immutable from 'immutable'
 
 export class App extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {
-      a: '',
-      b: '',
-      isValid: false,
-      errors: {
-      }
-    }
-  }
-  errorMessage(name) {
-    return this.state.errors[name]
+    this.state = {}
   }
 
-  change(name ,e) {
-    this.setState({[name]: e.target.value}, this.validate)
-  }
-
-  validate() {
-    let errors = {}
-    if(!this.state.a.length > 0)  errors.a = '必須です'
-    if(!this.state.b.length > 0)  errors.b = '必須です'
-    this.setState({isValid: Object.keys(errors).length == 0})
-    this.setState({errors: errors})
+  errorMessage() {
   }
 
   render() {
     return (
       <div>
-        <label>a</label><br/>
-        <input type='text' name='a'   onChange={this.change.bind(this, 'a')} validatable /><span></span><br />
+        <label>A</label><br/>
+        <input type='text' name='a'  validatable /><span></span><br />
         <span>{this.errorMessage('a')}</span><br/>
 
-        <label>b</label><br/>
-        <input type='text' ref='b' name='b' onChange={this.change.bind(this, 'b')}  validatable /><span></span><br />
+        <label>B</label><br/>
+        <input type='text' ref='b' name='b'  validatable /><span></span><br />
         <span>{this.errorMessage('b')}</span><br/>
 <br/>
-        <input type='submit' onClick={::this.validate} disabled={!this.state.isValid} /><br />
+        <input type='submit' /><br />
       </div>
     )
   }
--- ex-redux-validation/index.js
+++ ex-redux-validation/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { App } from './components/App'
+
+function reducer(state, action) {
+  // 本当はここでaction.typeで分岐して
+  // action.NAMEに格納される変数でstateを更新する
+  return state
+}
+
+let initialState = {counter: 0}
+let store = createStore(reducer, initialState)
 React.render(
-  <App />,
+  <Provider store={store}>
+    { () => <App /> }
+  </Provider>,
   document.getElementById('target')
 )
