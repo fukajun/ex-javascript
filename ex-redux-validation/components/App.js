17694a79ea80755ab5bcbb71cacf4329af0f74ff
J.Fukaya <fukajun.shark@gmail.com>
2015-08-30 21:38:43 +0900

    Implement validation

--- ex-redux-validation/components/App.js
+++ ex-redux-validation/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import _ from 'lodash'
 import Immutable from 'immutable'
+import { connect } from 'react-redux'
+import { INPUT } from '../constants/actionType'
+
+// Action & ActionCreator
+function input(name, value) {
+  return {
+    type: INPUT,
+    name,
+    value
+  }
+}
 
 export class App extends React.Component{
   constructor(props) {
@@ -8,22 +19,44 @@ export class App extends React.Component{
     this.state = {}
   }
 
-  errorMessage() {
+  errorMessage(name) {
+    var { error } = this.props
+    return error[name]
+  }
+
+  change(e) {
+    const { dispatch } = this.props
+    dispatch(input(e.target.name, e.target.value))
+  }
+  isValid(errros) {
+    const {  error: error } = this.props
+    return Object.keys(error).length == 0
   }
 
   render() {
+    const { main: { first: first, last: last }, error: { error: error } } = this.props
+
     return (
       <div>
+        <span>{this.errorMessage('all')}</span><br/>
+
         <label>A</label><br/>
-        <input type='text' name='a'  validatable /><span></span><br />
-        <span>{this.errorMessage('a')}</span><br/>
+        <input type='text' name='first' value={first} onChange={::this.change} />
+         <span>{this.errorMessage('first')}</span><br/>
 
         <label>B</label><br/>
-        <input type='text' ref='b' name='b'  validatable /><span></span><br />
-        <span>{this.errorMessage('b')}</span><br/>
-<br/>
-        <input type='submit' /><br />
+        <input type='text' name='last' value={last} onChange={::this.change} />
+        <span>{this.errorMessage('last')}</span><br/>
+
+        <br/>
+        <input type='submit' disabled={!this.isValid()}/><br />
       </div>
     )
   }
 }
+
+
+function map(state) {
+  return state
+}
+export default connect(map)(App)
--- ex-redux-validation/index.js
+++ ex-redux-validation/index.js
@@ -1,16 +1,51 @@
 import React from 'react'
 import { createStore } from 'redux'
+import _ from 'lodash'
 import { Provider } from 'react-redux'
-import { App } from './components/App'
+import App from './components/App'
+import { INPUT } from './constants/actionType'
 
-function reducer(state, action) {
-  // 本当はここでaction.typeで分岐して
-  // action.NAMEに格納される変数でstateを更新する
-  return state
+
+// Reducer
+function mainReducer(state, action) {
+  switch (action.type) {
+    case INPUT:
+      return _.assign({}, state, { [action.name]: action.value })
+    default:
+      return state
+  }
 }
 
-let initialState = {counter: 0}
-let store = createStore(reducer, initialState)
+function checkError(main) {
+  let errors = {}
+  if ((main.first.length +  main.last.length) < 10) {
+    errors['all'] = 'first + last  is shorten. need 10 charactors'
+  }
+  if (main.first.match(/[0-9]/)) {
+    errors['first'] = 'Could not include number'
+  }
+  if (main.last.match(/[0-9]/)){
+    errors['last'] = 'Could not include number'
+  }
+  return errors
+}
+
+function combine(state, action) {
+  let main = mainReducer(state.main, action)
+  let error = checkError(main)
+  return { main, error }
+}
+
+let initialState = { 
+  main: {
+    list: [],
+    first: 'junpay',
+    last: 'fukaya'
+  },
+  error: {
+  }
+}
+let store = createStore(combine, initialState)
 React.render(
   <Provider store={store}>
     { () => <App /> }
